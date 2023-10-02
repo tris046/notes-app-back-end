458ddc80d29e05862ca1e4fdc7a6a451a9d02e94
tri setyawati <trisetyawati11@student.uns.ac.id>
2023-10-02 14:44:10 +0700

    fix bugs host value 3

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 22,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
